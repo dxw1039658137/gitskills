53733f4d1d449d94aea58a87077648cfea51392b
dxw <1039658137@qq.com>
2020-11-24 21:56:52 +0800

    add new branch dev

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* GetLeanSkills()
+{
+	
+	return "1124 create new branch.";
+}
+
+int main()
+{
+	printf("hello world!\r\n");
+	return 0;
+	
+}
